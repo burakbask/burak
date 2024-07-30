6b2578fe7cadb09958b0655990c3552539b75ab8
Burak Can BASKIN <burakcanbaskin2@gmail.com>
2024-07-30 14:46:59 +0300

    Update image URLs for fizikfinito.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,12 +13,12 @@ export default function Index() {
       <div className="flex justify-center items-center space-x-12">
       <Link to="/fizikfinito">
       <div className="text-center">
-          <img src="/app/images/fizikfinito.webp" alt="Logo 1" className="h-32" />
+          <img src="/public/images/fizikfinito.webp" alt="Logo 1" className="h-32" />
           <p className="text-xl font-semibold mt-2">Fizikfinito</p>  {/* Logo 1 için açıklama */}
         </div>
          </Link>
         <div className="text-center ">
-          <img src="/app/images/biyikli.png" alt="Logo 2" className="h-32 mx-auto" />
+          <img src="/public/images/biyikli.png" alt="Logo 2" className="h-32 mx-auto" />
           <p className="text-xl font-semibold mt-2">Bıyıklı Matematik</p>  {/* Logo 2 için açıklama */}
         </div>
       </div>
